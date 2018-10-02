8e8840cc6380b1bf0a7d0cf95d6a35dabc010a09
joaoderocha <jharocha@gmail.com>
2018-10-01 21:05:20 -0300

    Interface Update

--- src/mainpkg/Main.java
+++ src/mainpkg/Main.java
@@ -1,9 +1,14 @@
 package mainpkg;
 
+import kernel.mode.KernelMode;
+import kernel.utils.LoadClass;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		KernelMode interf = (KernelMode) LoadClass.loadInstance("mode");
+		interf.execute();
 
 	}
 
